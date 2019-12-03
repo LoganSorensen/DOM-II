6eed1726445b5737ba493e489c4c4aac05cac4a5
Logan Sorensen <loganmsorensen@gmail.com>
2019-12-03 13:33:44 -0800

    added mouse-over and click events to images

--- js/index.js
+++ js/index.js
@@ -1,1 +1,32 @@
 // Your code goes here
+const contentImg = document.querySelectorAll(".img-content").forEach(contentImg => {
+
+    contentImg.addEventListener("mouseenter", () => {
+        contentImg.style.transform ="scale(1.2)";
+        contentImg.style.transition = "ease 3s";
+    })
+
+    contentImg.addEventListener("mouseout", () => {
+        contentImg.style.transform = "scale(1)";
+    })
+})
+
+const bottomImg = document.querySelector(".content-destination img");
+
+bottomImg.addEventListener("click", () => {
+    bottomImg.style.transform = "scale(0.5)";
+})
+
+bottomImg.addEventListener("dblclick", () => {
+    bottomImg.style.transform = "scale(1)";
+})
+
+
+
+// const navImg = document.querySelector("header img");
+//     navImg.style.width = "100px";
+//     navImg.style.marginTop = "6%";
+//     navImg.style.transform = "scaleX(-1)";
+//     navImg.addEventListener("scroll", () => {
+//         navImg.style.translateX = "200px";
+//     })
