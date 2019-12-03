e1ef7b34f6d6cede1cd212f6461bf0b2dcb8aff2
Logan Sorensen <loganmsorensen@gmail.com>
2019-12-03 14:09:55 -0800

    added wheel and keydown events

--- js/index.js
+++ js/index.js
@@ -21,6 +21,22 @@ bottomImg.addEventListener("dblclick", () => {
     bottomImg.style.transform = "scale(1)";
 })
 
+const headerImg = document.querySelector(".intro img");
+
+headerImg.addEventListener("wheel", (event) => {
+    let scale = 1;
+    event.preventDefault();
+
+    scale += event.deltaY * -0.01;
+    scale = Math.min(Math.max(.5, scale), 1);
+
+    headerImg.style.transform = `scale(${scale})`;
+})
+
+document.addEventListener("keydown", () => {
+    alert("What could you possibly be trying to type?");
+})
+
 
 
 // const navImg = document.querySelector("header img");
